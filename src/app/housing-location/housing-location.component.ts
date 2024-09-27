99e2cdca118bb821a7a9670089d4ef04e6f540a3
Alan Moore <alan@olpt.net>
2024-09-26 21:25:30 -0700

    add housing location with sample data

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housinglocation";
 
+/** Interpolation -- put {{ housingLocation.name }} in the markup to interpolate the data into the template  */
 @Component({
   selector: "app-housing-location",
   standalone: true,
   imports: [],
-  template: ` <p>housing-location works!</p> `,
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
   styleUrls: ["housing-location.component.css"],
 })
-export class HousingLocationComponent {}
+export class HousingLocationComponent {
+  /** non-null assertion, the '!' tells Angular that this value will not be null or undefined */
+  /** @Input passes input parameter through. */
+  @Input() housingLocation!: HousingLocation;
+}
